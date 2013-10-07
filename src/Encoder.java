c40f944edbb85754e48216e34ca3322adef1708b
Zazador <zazador@gmail.com>
2013-10-07 17:10:34 -0500

    lolz fixed bad math

--- src/Encoder.java
+++ src/Encoder.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 
 public class Encoder {
 	
-	static ArrayList<Integer> freqManager;
 	static int totalSum;
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		File file1 = new File(args[0]);
-		int freq = Integer.parseInt(args[1]);
+		//int freq = Integer.parseInt(args[1]);
+		ArrayList<Integer> freqManager = new ArrayList<Integer>();
 		
 		Scanner scan = new Scanner(file1);
 		while (scan.hasNextLine()) {
 			int curLine = Integer.parseInt(scan.nextLine());
 			freqManager.add(curLine);
+			System.out.println(curLine);
 		}
 		totalSum = getFreqSize(freqManager);
 		scan.close();
@@ -30,6 +31,7 @@ public class Encoder {
 		for (int i = 0; i < frequencies.size(); i++) {
 			sum += frequencies.get(i);
 		}
+		System.out.println(sum);
 		return sum;		
 	}
 
