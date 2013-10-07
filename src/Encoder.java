1f9579acf1fcfaf51475a52fb6e28ac8b0f6731a
Zazador <zazador@gmail.com>
2013-10-07 17:03:43 -0500

    just read in some frequencies

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,10 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class Encoder {
+	
+	static ArrayList<Integer> freqManager;
+	static int totalSum;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		File file1 = new File(args[0]);
+		int freq = Integer.parseInt(args[1]);
+		
+		Scanner scan = new Scanner(file1);
+		while (scan.hasNextLine()) {
+			int curLine = Integer.parseInt(scan.nextLine());
+			freqManager.add(curLine);
+		}
+		totalSum = getFreqSize(freqManager);
+		scan.close();
 
-
-	public static void main(String[] args) {
-
-
+	}
+	
+	public static int getFreqSize (ArrayList<Integer> frequencies) {
+		int sum = 0;
+		
+		for (int i = 0; i < frequencies.size(); i++) {
+			sum += frequencies.get(i);
+		}
+		return sum;		
 	}
 
 }
