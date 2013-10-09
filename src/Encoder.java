5f6e9e79286e696e548a264d9fd9ea0f010202a3
Zazador <zazador@gmail.com>
2013-10-08 19:47:34 -0500

    Finished creating testText

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,16 +1,23 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Encoder {
 	
 	static int totalSum;
+	static int k;
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		File file1 = new File(args[0]);
+		k = Integer.parseInt(args[1]);
 		//int freq = Integer.parseInt(args[1]);
 		ArrayList<Integer> freqManager = new ArrayList<Integer>();
 		
@@ -31,7 +38,47 @@ public class Encoder {
 		scan.close();
 
 		Huffman.processFile(freqManager);
+		try {
+			stepThree(probManager);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public static void stepThree(ArrayList<Double> probManager) throws IOException {
+		System.out.println("TEST");
+		//PrintWriter out = new PrintWriter("testText");
+		BufferedWriter out = new BufferedWriter(new FileWriter("testText.txt"));
+		int i, j, ran;
+		Random rand = new Random();
+		double newRan, counter = 0;
+		
+		for (i = 0; i < k; i++) {
+			ran = rand.nextInt(totalSum - 1);
+			newRan = (double) ran/ (double) 100;
+			
+			for (j = 0; j < probManager.size(); j++) {
+				double temp = (double) (probManager.get(j) + counter);
+				//System.out.println(temp);
+				if (newRan >= counter && newRan <= temp) {
+					int letter = 65 + j;
+					char ch = (char) letter;
+					out.write(ch);
+					//out.newLine();
+					break;
+				}
+				counter += probManager.get(j);
+			}
+			counter = 0;
+			
+		}
+		
+		out.close();
+		
+	}
+	
+	
 	
 	public static int getFreqSize (ArrayList<Integer> frequencies) {
 		int sum = 0;
